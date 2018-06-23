dafdf01dbeec7caefbef6e9db14a3058d40f14aa
Orlando Romero <oromero@users.noreply.github.com>
2018-06-24 00:14:57 +0200

    Add support for backing up and restoring the settings file

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+VSCODE_SETTINGS_FILENAME=/Users/orlando/Library/Application\ Support/Code/User/settings.json
 
 code --list-extensions > vscode-packages.txt
+cp "$VSCODE_SETTINGS_FILENAME" .
--- restore.sh
+++ restore.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+VSCODE_SETTINGS_FILENAME=/Users/orlando/Library/Application\ Support/Code/User/settings.json
 
 while read -r LINE; do
     code --install-extension $LINE
 done < vscode-packages.txt
+
+cp settings.json "$VSCODE_SETTINGS_FILENAME"
