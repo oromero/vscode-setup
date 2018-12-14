6e6ec7c96287dcd48f5c1cc46e0e2affd9486d6f
Orlando Romero <oromero@users.noreply.github.com>
2018-12-14 10:51:34 +0100

    Start using a global snippets configuration to be able to use snippets for both JavaScript and TypeScript files

--- backup.sh
+++ backup.sh
@@ -3,4 +3,4 @@ USER_SETTINGS_PATH=~/Library/Application\ Support/Code/User/
 
 code --list-extensions > vscode-packages.txt
 cp "${USER_SETTINGS_PATH}settings.json" .
-cp "${USER_SETTINGS_PATH}snippets/javascript.json" .
+cp "${USER_SETTINGS_PATH}snippets/global.code-snippets" .
